9a25c24fae2aab14b3dedff98bae0fdb5d308139
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2025-03-06 05:43:04 +0700

    initial commit after code generation

---
+++ internal/module/pet/config/generate.go
@@ -0,0 +1,4 @@
+package config
+
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config config.models.yaml ../../openapi.yaml
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config config.server.yaml ../../openapi.yaml
